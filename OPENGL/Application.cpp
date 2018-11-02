05d060045c2cdd9da6921bab95e23f455e179f7e
jinjuehui <jinjuehui@outlook.com>
2018-11-02 12:23:27 +0100

    add basic.shader and delete code in aplication.cpp

--- OPENGL/Application.cpp
+++ OPENGL/Application.cpp
@@ -102,25 +102,6 @@ int main(void)
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-	std::string vertexShader =
-		"#version 410 core \n"
-		"\n"
-		"layout(location = 0) in vec4 position;"
-		"\n"
-		"void main()\n"
-		"{\n"
-		"gl_Position = position;\n "
-		"}\n";
-
-	std::string fragmentShader =
-		"#version 410 core \n"
-		"\n"
-		"layout(location = 0) out vec4 color;"
-		"\n"
-		"void main()\n"
-		"{\n"
-		"color = vec4(1.0, 0.0, 0.0, 1.0); \n "
-		"}\n";
 
 	unsigned int shader = CreateShader(vertexShader, fragmentShader);
 	glUseProgram(shader);
