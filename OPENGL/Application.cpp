1078afc76c741ffa4cfeab3060a638ada1ce5b57
jinjuehui <jinjuehui@outlook.com>
2018-11-02 13:00:49 +0100

    add shader parser function ParseShader()

--- OPENGL/Application.cpp
+++ OPENGL/Application.cpp
@@ -1,8 +1,42 @@
 #include<GL/glew.h>
 #include<GLFW/glfw3.h>
 #include<iostream>
+#include<fstream>
+#include<string>
+#include<sstream>
 
+static void ParseShader(const std::string& filepath)
+{
+	std::ifstream stream(filepath);
 
+	enum class ShaderType
+	{
+		NONE = -1, VERTEX = 0, FRAGMENT = 1
+	};
+
+	std::string line;
+	std::stringstream ss[2];
+	ShaderType type = ShaderType::NONE;
+
+	while (getline(stream, line))
+	{
+		if (line.find("#shader") != std::string::npos) //std::string::npos  invalid string position
+		{
+			if (line.find("vertex") != std::string::npos)
+			{// set mode to vertex
+				type = ShaderType::VERTEX;
+			}
+			else if (line.find("fragment") != std::string::npos)
+			{//set mode to fragment
+				type = ShaderType::FRAGMENT;
+			}
+		}
+		else
+		{
+			ss[(int)type] << line << '\n';
+		}
+	}
+}
 
 static unsigned int CompileShader(unsigned int type , const std::string& source)
 {
