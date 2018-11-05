b8601aa8ce75958fa8e7c4bdb181c83447b55631
jinjuehui <jinjuehui@outlook.com>
2018-11-05 17:31:53 +0100

    add check funtion to check whether the shader compilation generate successuflly

--- OPENGL/gettingstarted.cpp
+++ OPENGL/gettingstarted.cpp
@@ -1,3 +1,7 @@
+//State machine
+//Objects: a collection of options that represents a subset of OpenGL's state
+//shaders: small programm on on GPU for process graphics pipeline.:
+//         written in OpenGGL Shading Language(GLSL)
 #include <GL/glew.h>	
 #include <GLFW/glfw3.h>
 
@@ -12,6 +16,12 @@
 	ASSERT(error_callback(#x,__FILE__, __LINE__))
 
 
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
 static const struct
 {
 	float x, y;
@@ -20,7 +30,7 @@ static const struct
 
 vertices[3] = 
 {
-	{-0.6f,-0.4f,1.0f,0.f,0.f},
+	{-0.6f,-0.4f,1.0f,0.f,0.f},  //3d coordnate, r,g,b
 	{0.6f,-0.4f,0.f,1.f,0.f},
 	{0.f,0.6f,0.f,0.f,1.f}
 
@@ -57,7 +67,7 @@ static bool error_callback(const char* function, const char* file, int line)
 {
 	while (GLenum error = glGetError())
 	{
-		std::cout << "[OpenGL Error] (" << error << "): " << function << " " << file << ": " << line << std::endl;
+		std::cout << "[OpenGL Error] (" <<std::hex<< error << "): " << function << " " << file << ": " << line << std::endl;
 		return false;
 	}
 	return true;
@@ -77,13 +87,13 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 int main(void)
 {
 	GLFWwindow* window;
-	unsigned int vertex_buffer, vertex_shader, fragment_shader, program;
+	unsigned int vertex_buffer, vertex_shader, fragment_shader, program;//define object
 	int mvp_location, vpos_location, vcol_location;
 
 	if (!glfwInit())
 		exit(EXIT_FAILURE);
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
 	window = glfwCreateWindow(640, 480, "Simple Example", NULL, NULL);
@@ -104,12 +114,12 @@ int main(void)
 
 	glfwSwapInterval(1);
 
-	GLCall(glGenBuffers(1, &vertex_buffer));
-	GLCall(glBindBuffer(GL_ARRAY_BUFFER,vertex_buffer));
-	GLCall(glBufferData(GL_ARRAY_BUFFER,sizeof(vertices), vertices, GL_STATIC_DRAW));
-
+	GLCall(glGenBuffers(1, &vertex_buffer));//create buffer object, the buffer type of a vertex buffer object is GL_ARRAY_BUFFER
+	GLCall(glBindBuffer(GL_ARRAY_BUFFER,vertex_buffer));//bind object to context
+	GLCall(glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW));//Copy the previously defined vertes data into the buffer's memory
+	//					1.type of buffer,  2.bytes, 3.actual data, 4.the data will most likely not change at all. enum{GL_STATIC_DRAW, GL_DYNAMIC_DRAW, GL_STREAM_DRAW}           
 	GLCall(vertex_shader = glCreateShader(GL_VERTEX_SHADER));
-	GLCall(glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL));
+	GLCall(glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL));// 1.shader object, 2.how many strings we're passing as source code, 3.actual source code, 4...not explained
 	GLCall(glCompileShader(vertex_shader));
 
 	GLCall(fragment_shader = glCreateShader(GL_FRAGMENT_SHADER));
@@ -131,6 +141,29 @@ int main(void)
 	GLCall(glEnableVertexAttribArray(vcol_location));
 	GLCall(glVertexAttribPointer(vcol_location, 3, GL_FLOAT,GL_FALSE,sizeof(float)*5,(void*)(sizeof(float)*2)));
 
+	//check whether the two shader compiled successfully
+	int vertex_success, fragment_success;
+	char infoLog[512];
+	glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &vertex_success);
+	glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &fragment_success);
+
+	if (!vertex_success || !fragment_success)
+	{
+		if (!vertex_success)
+		{
+			glGetShaderInfoLog(vertex_shader, 512, NULL, infoLog);
+			std::cout << "ERROR::SHADER::VERTEX::COMPILATTION_FAILED\n" << infoLog << std::endl;
+		}
+		else
+		{
+			glGetShaderInfoLog(fragment_shader,512,NULL, infoLog);
+			std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+		}
+	}
+	
+
+	//GLCall(glfwSetFramebufferSizeCallback(window, framebuffer_size_callback));
+
 	while (!glfwWindowShouldClose(window))
 	{
 		float ratio;
@@ -140,8 +173,10 @@ int main(void)
 		GLCall(glfwGetFramebufferSize(window, &width, &height));
 		ratio = width / (float)height;
 
+		//GLCall(glfwSetFramebufferSizeCallback(window, framebuffer_size_callback));
+		GLCall(glClearColor(0.0f,0.0f,0.0f,0.0f));//state-setting: set background color
+		GLCall(glClear(GL_COLOR_BUFFER_BIT));//state-using: retrieve the function in the current state
 		GLCall(glViewport(0, 0, width, height));
-		GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
 		mat4x4_identity(m);
 		mat4x4_rotate_Z(m,m,(float) glfwGetTime());
