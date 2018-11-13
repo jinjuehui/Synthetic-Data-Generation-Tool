7bbc8c79d8da7e124fceade4d79daec244c9ff5f
Juehui Jin <jinjuehui@outlook.com>
2018-11-13 14:20:15 +0100

    create the renderer headers, and move the debug informations to the headers

---
+++ OPENGL/Renderer.cpp
@@ -0,0 +1,20 @@
+#include "Renderer.h"
+#include <GL/glew.h>
+#include <iostream>
+
+
+void GLClearError()
+{
+	while (glGetError() != GL_NO_ERROR);
+}
+
+bool GLLogCall(const char* function, const char* file, int line)
+{
+	while (GLenum error = glGetError())
+	{
+		std::cout << "[OpenGL Error] (" << std::hex << error << "): " << function <<
+			" " << file << ":" << line << std::endl;
+		return false;
+	}
+	return true;
+}
--- OPENGL/drawtiangle.cpp
+++ OPENGL/drawtiangle.cpp
@@ -1,26 +1,7 @@
 #include <GL/glew.h>
 #include<GLFW/glfw3.h>
 #include<iostream>
-
-#define ASSERT(x) if (!(x)) __debugbreak();
-#define GLCall(x) GLClearError();\
-	x;\
-	ASSERT(GLLogCall(#x, __FILE__, __LINE__))
-static void GLClearError()
-{
-	while (glGetError() != GL_NO_ERROR);
-}
-
-static bool GLLogCall(const char* function, const char* file, int line)
-{
-	while (GLenum error = glGetError())
-	{
-		std::cout << "[OpenGL Error] (" <<std::hex<< error << "): " << function <<
-			" " << file << ":" << line << std::endl;
-		return false;
-	}
-	return true;
-}
+#include"Renderer.h"
 
 
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod)
@@ -45,9 +26,10 @@ const char* vertex_shader_source = "#version 330 core\n"
 	"}\0";;
 const char* fragment_shader_source = "#version 330 core\n"
     "out vec4 FragColor;\n"
+	"uniform vec4 color;\n"
     "void main()\n"
     "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "   FragColor = color;\n"
 	"}\n\0";;
 
 const unsigned int SCR_WIDTH = 800;
@@ -63,9 +45,9 @@ int main()
 		exit(EXIT_FAILURE);
 	
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);//Why it doesn't work with 2,3
-	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);//what does this influence
-
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);//Why it doesn't work with 2,3
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);//what does this influence
+											//if core_profile is chosen, the vertex array object need to be manually created
 	window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "TRIANGLE",NULL,NULL);
 	if (!window)
 	{
@@ -81,8 +63,15 @@ int main()
 		std::cout << "initialze glew failed" << std::endl;
 	}
 	
+	int nrAttributes;
+	unsigned int vbo, vao, vertex_shader, fragment_shader, program;
 
-	unsigned int vbo, vertex_shader, fragment_shader, program;
+	glGetIntegerv(GL_MAX_VERTEX_ATTRIBS,&nrAttributes);
+	std::cout << "maximum number of vertex attributes supported: " << nrAttributes << std::endl;
+	
+// Generate Vertex Array Object, useful when dealing with multiple vbo layouts
+	GLCall(glGenVertexArrays(1,&vao));
+	GLCall(glBindVertexArray(vao));
 
 //2.Generate Buffer============================================================================
 	GLCall(glGenBuffers(1, &vbo));
@@ -115,34 +104,51 @@ int main()
 		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED" << std::endl;
 	}
 
-
-//4.create program
-	GLCall(program = glCreateProgram());
-	GLCall(glAttachShader(program, vertex_shader));
-	GLCall(glAttachShader(program,fragment_shader));
-	GLCall(glLinkProgram(program));
-	GLCall(glGetProgramiv(program,GL_LINK_STATUS,&success));
-	if (!success)
-	{
-		glGetProgramInfoLog(program, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED";
-	}
-
-	glDeleteShader(vertex_shader);
-	glDeleteShader(fragment_shader);
-
 	
-//5. while loop	
-	while (!glfwWindowShouldClose(window))
-	{
-		GLCall(glClearColor(0.2f,0.3f,0.3f,1.0f));
-		GLCall(glClear(GL_COLOR_BUFFER_BIT));
-		GLCall(glUseProgram(program));
-		GLCall(glDrawArrays(GL_TRIANGLES,0,3));
-		GLCall(glfwSwapBuffers(window));
-		GLCall(glfwPollEvents());
+		//4.create program
+		GLCall(program = glCreateProgram());
+		GLCall(glAttachShader(program, vertex_shader));
+		GLCall(glAttachShader(program, fragment_shader));
+		GLCall(glLinkProgram(program));
+		GLCall(glGetProgramiv(program, GL_LINK_STATUS, &success));
+		if (!success)
+		{
+			glGetProgramInfoLog(program, 512, NULL, infoLog);
+			std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED";
+		}
 
-	}
+		int fragment_color;
+		fragment_color = glGetUniformLocation(program, "color");
+		GLCall(glUseProgram(program));
+
+		GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+		GLCall(glUseProgram(0));
+		GLCall(glBindVertexArray(0));
+
+		//glDeleteShader(vertex_shader);
+		//glDeleteShader(fragment_shader);
+		float color = 1.0f, r = 0.01f;
+
+		glfwSwapInterval(1);
+
+		//5. while loop	
+		while (!glfwWindowShouldClose(window))
+		{
+			GLCall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
+			GLCall(glClear(GL_COLOR_BUFFER_BIT));
+			GLCall(glUseProgram(program));
+			GLCall(glUniform4f(fragment_color, color, -color, 0.5, 1.0f));
+
+			if (color >= 1 || color <= 0)
+				r = -r;
+
+			color += r;
+			GLCall(glBindVertexArray(vao))
+				GLCall(glDrawArrays(GL_TRIANGLES, 0, 3));
+			GLCall(glfwSwapBuffers(window));
+			GLCall(glfwPollEvents());
+
+		}
 	glfwTerminate();
 	exit(EXIT_SUCCESS);
 	return 0;
